34cc7e906651c8456088b36da4dd25174dd1ddd2
reshma-a84 <reshu.a84@gmail.com>
2023-03-17 21:11:14 +0530

    March 17 2023 Day 16

---
+++ Day-17/Day17-Class.py
@@ -0,0 +1,29 @@
+class User:
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
+
+# user_1 = User()
+# user_1.id = "001"
+# user_1.username = "Angela"
+
+user_1 = User("001", "angela")
+user_2 = User("002", "Jules")
+# print(user_1.id)
+# print(user_1.username)
+# print(user_1.follower)
+# print(user_2.id)
+# print(user_2.username)
+
+user_1.follow(user_2)   # Here user1 is following user 2
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
---
+++ Day-17/Quiz/question_model.py
@@ -0,0 +1,6 @@
+class Question:
+    def __init__(self, q_text, q_answer):
+        self.text = q_text            # When we create attributes we HAVE to use it with 'self'
+        self.answer = q_answer
+
+
