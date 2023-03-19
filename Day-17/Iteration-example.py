fb5e5310fa9c13b9acbf1c2db9b812e0326f5922
reshma-a84 <reshu.a84@gmail.com>
2023-03-19 12:25:33 +0530

    March 19 2023 - iteration-example.py to test out OOPS concept

--- Day-17/Iteration-example.py
+++ Day-17/Iteration-example.py
@@ -2,16 +2,16 @@ class User:
     def __init__(self, userid):
         self.id = userid
 
-    def print_name(self, userid):
+    def print_userid(self, userid):
         self.id = userid
         print(f"Userid{self.id} ")
 
 
 usr = User(id)     # passing parameter to Class is important
-
+itr = int(input("How many iterations do you want? : "))
 i = 0
-while i < 5:
+while i < itr:
 
     usr.id = i + 1
-    usr.print_name(usr.id)
+    usr.print_userid(usr.id)
     i += 1
